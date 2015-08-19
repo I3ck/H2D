f36d5e09883f949d998e6166beaf10451a5fe998
Martin Buck <buckmartin@buckmartin.de>
2015-08-19 22:21:59 +0200

    added more classes

--- Vec2D.hs
+++ Vec2D.hs
@@ -5,30 +5,31 @@ data Vec2D = Vec2D {
     y :: Double
 } deriving (Show, Read)
 
+--Todo leave data in files, move classes to own files and all instances to the corresponding class file
 class Moveable a where
     move :: a -> Vec2D -> a
 
+class Mirrorable a where
+    mirrorV :: a -> Double -> a
+    mirrorH :: a -> Double -> a
+    mirrorP :: a -> Vec2D -> a
+
+class Rotateable a where
+    rotate :: a -> Double -> Vec2D -> a
+
 instance Moveable Vec2D where
     move (Vec2D a b) (Vec2D x y) = Vec2D (a+x) (b+y)
-    
---------------------------------------------------------------------------------
 
-mirrorV :: Vec2D -> Double -> Vec2D
-mirrorV (Vec2D x y) a = Vec2D(2 * a - x) y
+instance Mirrorable Vec2D where
+    mirrorV (Vec2D x y) a           = Vec2D (2 * a - x) y
+    mirrorH (Vec2D x y) a           = Vec2D x (2 * a - y)
+    mirrorP (Vec2D x y) (Vec2D a b) = Vec2D (2 * a - x) (2 * b - y)
 
-mirrorH :: Vec2D -> Double -> Vec2D
-mirrorH (Vec2D x y) a = Vec2D x (2 * a - y)
-
-mirrorP :: Vec2D -> Vec2D -> Vec2D
-mirrorP (Vec2D x y) (Vec2D a b) = Vec2D(2 * a - x) (2 * b - y)
-
---------------------------------------------------------------------------------
-
-rotate :: Vec2D -> Double -> Vec2D -> Vec2D
-rotate (Vec2D x y) rad (Vec2D centerX centerY) = Vec2D newX newY
-    where
-        newX = centerX + cos rad * (x - centerX) - sin rad * (y - centerY)
-        newY = centerY + sin rad * (x - centerY) + cos rad * (y - centerY)
+instance Rotateable Vec2D where
+    rotate (Vec2D x y) rad (Vec2D centerX centerY) = Vec2D newX newY
+        where
+            newX = centerX + cos rad * (x - centerX) - sin rad * (y - centerY)
+            newY = centerY + sin rad * (x - centerY) + cos rad * (y - centerY)
 
 --------------------------------------------------------------------------------
 
