8ef625a25a1274e857692acdaa00d5544e3de6d3
Martin Buck <buckmartin@buckmartin.de>
2015-10-15 21:54:20 +0200

    started working on a KdTree

---
+++ src/KdTree2D.hs
@@ -0,0 +1,24 @@
+{-
+Copyright 2015 Martin Buck
+This file is part of H2D.
+H2D is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+H2D is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with H2D.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+module KdTree2D where
+
+data KdTree2D v = Node { left   :: KdTree2D v
+                       , val    :: v
+                       , right  :: KdTree2D v
+                       , axis   :: Int
+                       }
+                  | Empty
+    deriving (Eq, Ord, Show)
